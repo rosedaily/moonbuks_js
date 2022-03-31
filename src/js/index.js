1444b3580a8e4a6cbe297561d4e49c63ee2652e6
rose <rosedaily1001@gmail.com>
2022-04-01 06:29:17 +0900

    MenuApi 객체분리 - 메뉴생성, 수정까지

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@ import { $ } from "./utils/dom.js";
 import store from "./store/index.js";
 
 // TODO 서버 요청부분
-// - [] 웹서버를 띄운다.  
-// - [] 서버에 새로운 메뉴가 추가될 수 있도록 요청한다.
-// - [] 서버에 카테고리별 메뉴리스트를 불러온다,
-// - [] 서버에 메뉴가 수정 될 수 있도록 요청한다.
+// - [o] 웹서버를 띄운다.  
+// - [o] 서버에 새로운 메뉴가 추가될 수 있도록 요청한다.
+// - [o] 서버에 카테고리별 메뉴리스트를 불러온다,
+// - [o] 서버에 메뉴가 수정 될 수 있도록 요청한다.
 // - [] 서버에 메뉴의 품절상태가 토글될 수 있도록 요청한다.
 // - [] 서버에 메뉴가 삭제될 수 있도록 요청한다.
 
@@ -18,6 +18,43 @@ import store from "./store/index.js";
 // - [] 중복되는 메뉴는 추가할 수 없다.
 const BASE_URL = "http://localhost:3000/api"
 // fetch('/', option); //요청을 보내기 위한 url, 요청하는 옵션들
+const MenuApi = {
+  async getAllMenuByCategory(category){
+    const response = await fetch(`${BASE_URL}/category/${category}/menu`)
+      return response.json();
+  },
+  async createMenu(category, name){
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu`,
+      {
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json",
+        },
+        body:JSON.stringify({name}),
+      }
+    );
+    if(!response.ok){
+      console.error("에러가 발생했습니다.");
+    }
+  }, 
+  async updateMenu(category, name, menuId){
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      {
+        method:"PUT",
+        headers:{
+          "Content-Type":"application/json",
+        },
+        body:JSON.stringify({name}),
+      }
+    );
+    if(!response.ok){
+      console.error("에러가 발생했습니다.");
+    }
+    return response.json();
+  }
+};
 
 function App() {
   this.menu = {
@@ -28,10 +65,11 @@ function App() {
     desert: [],
   };
   this.currentCategory = "espresso";
-  this.init = () => {
-    if(store.getLocalStorage()){
-      this.menu = store.getLocalStorage();
-    }
+
+  this.init = async () => {
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+     );
     render();
     initEventListeners();
   }
@@ -40,7 +78,7 @@ function App() {
     const template = this.menu[this.currentCategory]
     .map((menuItem, index) => {
       return `
-        <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+        <li data-menu-id="${menuItem.id}" class="menu-list-item d-flex items-center py-2">
           <span class="${menuItem.soldOut ? "sold-out" : "" } w-100 pl-2 menu-name">${menuItem.name}</span>
           <button
             type="button"
@@ -81,45 +119,26 @@ function App() {
       alert("값을 입력해주세요.");
       return;
     }
-
     const menuName = $("#menu-name").value;
-    // this.menu[this.currentCategory].push({ name: menuName }); // api를 사용해서 웹서버 등록
-    await fetch(`${BASE_URL}/category/${this.currentCategory}/menu`,{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify({name:menuName}),
-    })
-    .then((response) => {
-      return response.json();
-      })
+    await MenuApi.createMenu(this.currentCategory, menuName);
 
-    await fetch(`${BASE_URL}/category/${this.currentCategory}/menu`)
-      .then((response) => {
-      return response.json();
-      })
-      .then((data) =>{
-        console.log(data);
-        this.menu[this.currentCategory] = data;
-        render();
-        $("#menu-name").value = "";
-      });
-    
-    store.setLocalStorage(this.menu);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
     $("#menu-name").value = "";
-
-    // console.log(menuItemTemplate(menuName));
   };
 
-  const updateMenuName = (e) => {
+  const updateMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요.", $menuName.innerText);
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
-    store.setLocalStorage(this.menu);
-    // $menuName.innerText = updatedMenuName; //render로 관리
+    await MenuApi.updateMenu(this.currentCategory, updatedMenuName, menuId)
+
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
+
     render();
   }
 
@@ -172,13 +191,16 @@ function App() {
       addMenuName();
     });
   
-    $("nav").addEventListener("click", (e) => {
+    $("nav").addEventListener("click", async (e) => {
       const isCategoryButton = e.target.classList.contains("cafe-category-name");
       if(isCategoryButton){
         const categoryName = e.target.dataset.categoryName; 
         this.currentCategory = categoryName;
         console.log("::카테고리::"+categoryName);
         $("#category-title").innerText = `${e.target.innerText} 메뉴관리`;
+        this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+          this.currentCategory
+        );
         render();
       }
   
